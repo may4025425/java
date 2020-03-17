e0dbee50149f5594808ca969a56eeb172d20b605
may4025425 <may4025425@gmail.com>
2020-03-17 11:55:52 +0800

    3/17 兩個try&catch

---
+++ src/main/java/com/may/Billing.java
@@ -0,0 +1,12 @@
+package com.may;
+
+import java.util.Scanner;
+
+public class Billing {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Total:");
+        int total = Integer.parseInt(scanner.nextLine());
+        System.out.println();
+    }
+}
