cab3bdfc21a6cd35e786963706e9839664aa0282
may4025425 <may4025425@gmail.com>
2020-03-17 11:39:33 +0800

    3/17 valid

--- src/main/java/com/may/Hello.java
+++ src/main/java/com/may/Hello.java
@@ -9,8 +9,20 @@ public class Hello {
         System.out.println("Please enter your age!");
         Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();
-        int num = Integer.parseInt(s);
-        System.out.println(Math.sqrt(num));
-
+//        int num = Integer.parseInt(s);
+//        System.out.println(Math.sqrt(num));
+        boolean valid = true;
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if (c < '0' || c > '9') {
+                valid = false;
+            }
+        }
+        if(valid){
+            int age = Integer.parseInt(s);
+            System.out.println(Math.sqrt(age*10));
+        }else{
+            System.out.println("error");
+        }
     }
 }
