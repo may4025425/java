ff4eca771f7cae65562ab3b88317db93dd5dfcca
may4025425 <may4025425@gmail.com>
2020-03-17 11:47:36 +0800

    3/17 try&catch

---
+++ src/main/java/com/may/Hello2.java
@@ -0,0 +1,19 @@
+package com.may;
+
+import java.util.Scanner;
+
+public class Hello2 {
+    public static void main(String[] args) {
+        System.out.println("Please enter your age!");
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+//        int num = Integer.parseInt(s);
+        System.out.println(s);
+        try {
+            int age = Integer.parseInt(s);
+            System.out.println(Math.sqrt(age*10));
+        }catch (NumberFormatException e){
+            System.out.println("error");
+        }
+    }
+}
