f2bf4474aff0b37a38f9ea6cc0c5fe1cfd519ccf
may4025425 <may4025425@gmail.com>
2020-03-24 09:41:41 +0800

    上次的Billing.class

--- src/main/java/com/may/Billing.java
+++ src/main/java/com/may/Billing.java
@@ -7,6 +7,19 @@ public class Billing {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Total:");
         int total = Integer.parseInt(scanner.nextLine());
-        System.out.println();
+        System.out.println("Need to split bill?");
+        String yn = scanner.nextLine();
+        if(yn.equalsIgnoreCase("y")){
+            System.out.println("How many kids?");
+            try{
+                int n = Integer.parseInt(scanner.nextLine());
+                int eachPaid = total / n;
+                System.out.println(eachPaid + " for each ");
+            }catch (NumberFormatException e){
+                System.out.println("Hello?");
+            }catch (ArithmeticException e){
+                System.out.println("Hello");
+            }
+        }
     }
 }
