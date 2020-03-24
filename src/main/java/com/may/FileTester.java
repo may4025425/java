48fea62dda8e2a31483d95c9ece30f1b887b060f
may4025425 <may4025425@gmail.com>
2020-03-24 11:43:35 +0800

    3/24 FileWriter

--- src/main/java/com/may/FileTester.java
+++ src/main/java/com/may/FileTester.java
@@ -1,11 +1,25 @@
 package com.may;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FileTester {
     public static void main(String[] args) {
-        File file = new File("src");
+/*        File file = new File("src");
         System.out.println(file.exists());
         System.out.println(file.getAbsolutePath());
+        System.out.println(file.);
+ */
+        try {
+            FileWriter writer = new FileWriter("abc.txt");
+            writer.write(65);
+            writer.write(66);
+            writer.write("66,65");
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 }
