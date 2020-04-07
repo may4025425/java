4bd52c77040a461dbd927ff7f42b86eda1358dd5
may4025425 <may4025425@gmail.com>
2020-04-07 11:28:31 +0800

    4/7 BufferedReader

--- src/main/java/com/may/ReadTester.java
+++ src/main/java/com/may/ReadTester.java
@@ -1,24 +1,22 @@
 package com.may;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class ReadTester {
     public static void main(String[] args) {
         try {
             FileReader fr = new FileReader("test.txt");
-            int data = fr.read();
-            while (data !=-1){
-                System.out.println((char)data);
-                data = fr.read();
-            } //-1指的是空值
+            BufferedReader br = new BufferedReader(fr);
+            String line = br.readLine();
+            while (line !=null){
+                System.out.println(line);
+                line = br.readLine();
+            }
 
 /*  接下去讀  data = fr.read();
             System.out.println((char)data);
              data = fr.read();
-            System.out.println((char)data);
-            */
+            System.out.println((char)data); */
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
