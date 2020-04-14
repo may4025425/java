fd3abb238fc6cd93279b938c7766c4d70013cbc3
may4025425 <may4025425@gmail.com>
2020-04-14 10:06:26 +0800

    4/14 分辨投入1元,5元,10元的功能

--- src/main/java/com/may/PiggyBank.java
+++ src/main/java/com/may/PiggyBank.java
@@ -4,16 +4,28 @@ import java.util.Scanner;
 
 public class PiggyBank {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         String yesorno;
         int total = 0;
+        int i1 = 0,i5 = 0,i10 = 0;
         do{
             System.out.println("請投入1元,5元或10元硬幣（輸入1,5,10）");
             int money = Integer.parseInt(scanner.nextLine());
             if (money == 1 | money == 5 | money == 10) {
+                if(money==1){
+                     i1 = i1 + 1;
+                    System.out.println("投入的1元硬幣數量:"+i1);
+                } else if (money==5){
+                     i5 = i5 + 1;
+                    System.out.println("投入的5元硬幣數量:"+i5);
+                }else {
+                     i10 = i10 + 1;
+                    System.out.println("投入的10元硬幣數量:"+i10);
+                }
                 total = total + money;
                 System.out.println("目前的總和：" + total + "元");
-                System.out.println("是否繼續投幣？（yes or no)");
+                System.out.println("是否繼續投幣？（y or n)");
                 yesorno = scanner.nextLine();
             }else{
                 System.out.println("無法處理!");
@@ -32,7 +44,7 @@ public class PiggyBank {
         if (money == 1 | money == 5 | money == 10) {
             System.out.println("目前的總和： " + money + "元");
             while (money >= 1) {
-                System.out.println("是否繼續投幣？（yes or no)");
+                System.out.println("是否繼續投幣？（y or n)");
                 String yesorno = scanner.nextLine();
                 if (yesorno.equalsIgnoreCase("y")) {
                     System.out.println("請投入1元,5元或10元硬幣（輸入1,5,10）");
